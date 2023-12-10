e4e8fe038afcd91c223afb8fb1f3ccfc688c5bb7
Fredrik Orderud <fredrik.orderud@ge.com>
2023-12-10 23:09:19 +0100

    Replace DevCon with DevGen for (un)installation.
    Fixes #7.

--- simbatt/INSTALL.bat
+++ simbatt/INSTALL.bat
@@ -2,11 +2,15 @@
 :: Goto current directory
 cd /d "%~dp0"
 
-:: Use DevCon for installation, since it allows providing HWID
-devcon.exe /r install simbatt.inf "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+:: Install driver
+pnputil.exe /add-driver simbatt.inf /install /reboot
+
+:: Add batterie(s)
+devgen /add /instanceid 0000 /hardwareid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+devgen /add /instanceid 0001 /hardwareid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+devgen /add /instanceid 0002 /hardwareid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 
 :: Use PnpUtil for installation (succeeds but driver isn't loaded)
 ::devgen /add /hardwareid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
-::pnputil.exe /add-driver simbatt.inf /install /reboot
 
 pause
--- simbatt/UNINSTALL.bat
+++ simbatt/UNINSTALL.bat
@@ -2,13 +2,15 @@
 :: Goto current directory
 cd /d "%~dp0"
 
-:: delete the devnode
-devcon.exe /r remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+:: remove batterie(s)
+devgen.exe /remove SWD\DEVGEN\0000
+devgen.exe /remove SWD\DEVGEN\0001
+devgen.exe /remove SWD\DEVGEN\0002
+
 :: TODO: Switch to PNPUTIL in Win11 21H2
 ::pnputil.exe /remove-device /deviceid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 
 :: uninstall driver
-::devgen /remove /hardwareid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 pnputil.exe /delete-driver simbatt.inf /uninstall /force /reboot
 
 pause
