4e9887475188305acb7e5ebcb6b03b2f935148d2
Fredrik Orderud <fredrik.orderud@ge.com>
2023-12-18 10:00:07 +0100

    Introduce DeviceInstance wrapper class.

--- BatteryConfig/Main.cpp
+++ BatteryConfig/Main.cpp
@@ -9,66 +9,88 @@
 #include <vector>
 
 
-std::variant<std::wstring, FILETIME> GetDevInstProperty(DEVINST dnDevInst, const DEVPROPKEY& propertyKey) {
-    DEVPROPTYPE propertyType = 0;
-    std::vector<BYTE> buffer(1024, 0);
-    ULONG buffer_size = (ULONG)buffer.size();
-    CONFIGRET res = CM_Get_DevNode_PropertyW(dnDevInst, &propertyKey, &propertyType, buffer.data(), &buffer_size, 0);
-    if (res != CR_SUCCESS) {
-        wprintf(L"ERROR: CM_Get_DevNode_PropertyW (res=%i).\n", res);
-        return {};
-    }
-    buffer.resize(buffer_size);
 
-    if (propertyType == DEVPROP_TYPE_STRING) {
-        return reinterpret_cast<wchar_t*>(buffer.data());
-    } else if (propertyType == DEVPROP_TYPE_FILETIME) {
-        return *reinterpret_cast<FILETIME*>(buffer.data());
+
+class DeviceInstance {
+public:
+    DeviceInstance(wchar_t* deviceInstancePath) {
+        CONFIGRET res = CM_Locate_DevNodeW(&m_devInst, deviceInstancePath, CM_LOCATE_DEVNODE_NORMAL);
+        if (res != CR_SUCCESS)
+            throw std::runtime_error("ERROR: CM_Locate_DevNodeW");
     }
 
-    throw std::runtime_error("Unsupported CM_Get_DevNode_PropertyW type");
-}
-
-std::wstring FileTimeToDateStr(FILETIME& fileTime) {
-    SYSTEMTIME time = {};
-    BOOL ok = FileTimeToSystemTime(&fileTime, &time);
-    if (!ok) {
-        DWORD err = GetLastError();
-        wprintf(L"ERROR: FileTimeToSystemTime failure (res=%i).\n", err);
-        return {};
+    ~DeviceInstance() {
     }
 
-    std::wstring dateString(128, L'\0');
-    int char_count = GetDateFormatW(LOCALE_SYSTEM_DEFAULT, NULL, &time, NULL, dateString.data(), (int)dateString.size());
-    dateString.resize(char_count-1); // exclude zero-termination
-    return dateString;
-}
+    std::wstring GetDriverVersion() const {
+        auto res = GetDevInstProperty(DEVPKEY_Device_DriverVersion);
+        return std::get<std::wstring>(res);
+    }
+
+    FILETIME GetDriverDate() const {
+        auto res = GetDevInstProperty(DEVPKEY_Device_DriverDate);
+        return std::get<FILETIME>(res);
+    }
+
+    std::variant<std::wstring, FILETIME> GetDevInstProperty(const DEVPROPKEY& propertyKey) const {
+        DEVPROPTYPE propertyType = 0;
+        std::vector<BYTE> buffer(1024, 0);
+        ULONG buffer_size = (ULONG)buffer.size();
+        CONFIGRET res = CM_Get_DevNode_PropertyW(m_devInst, &propertyKey, &propertyType, buffer.data(), &buffer_size, 0);
+        if (res != CR_SUCCESS) {
+            wprintf(L"ERROR: CM_Get_DevNode_PropertyW (res=%i).\n", res);
+            return {};
+        }
+        buffer.resize(buffer_size);
+
+        if (propertyType == DEVPROP_TYPE_STRING) {
+            return reinterpret_cast<wchar_t*>(buffer.data());
+        }
+        else if (propertyType == DEVPROP_TYPE_FILETIME) {
+            return *reinterpret_cast<FILETIME*>(buffer.data());
+        }
+
+        throw std::runtime_error("Unsupported CM_Get_DevNode_PropertyW type");
+    }
+
+    static std::wstring FileTimeToDateStr(FILETIME fileTime) {
+        SYSTEMTIME time = {};
+        BOOL ok = FileTimeToSystemTime(&fileTime, &time);
+        if (!ok) {
+            DWORD err = GetLastError();
+            wprintf(L"ERROR: FileTimeToSystemTime failure (res=%i).\n", err);
+            return {};
+        }
+
+        std::wstring dateString(128, L'\0');
+        int char_count = GetDateFormatW(LOCALE_SYSTEM_DEFAULT, NULL, &time, NULL, dateString.data(), (int)dateString.size());
+        dateString.resize(char_count - 1); // exclude zero-termination
+        return dateString;
+    }
+
+    DEVINST m_devInst = 0;
+};
 
 
 /** Get the virtual file physical device object (PDO) path of a device driver instance. */
 static std::wstring GetPDOPath(wchar_t* deviceInstancePath) {
-    DEVINST dnDevInst = 0;
-    CONFIGRET res = CM_Locate_DevNodeW(&dnDevInst, deviceInstancePath, CM_LOCATE_DEVNODE_NORMAL);
-    if (res != CR_SUCCESS) {
-        wprintf(L"ERROR: CM_Locate_DevNodeW (res=%i).\n", res);
-        return {};
-    }
+    DeviceInstance dev(deviceInstancePath);
 
     {
         // get driver version
-        auto res = GetDevInstProperty(dnDevInst, DEVPKEY_Device_DriverVersion);
-        wprintf(L"Driver version: %s.\n", std::get<std::wstring>(res).c_str());
+        auto res = dev.GetDriverVersion();
+        wprintf(L"Driver version: %s.\n", res.c_str());
     }
     {
         // get driver date
-        auto res = GetDevInstProperty(dnDevInst, DEVPKEY_Device_DriverDate);
-        wprintf(L"Driver date: %s.\n", FileTimeToDateStr(std::get<FILETIME>(res)).c_str());
+        auto res = dev.GetDriverDate();
+        wprintf(L"Driver date: %s.\n", DeviceInstance::FileTimeToDateStr(res).c_str());
     }
 
     std::wstring pdoPath;
     {
         // get PDO path
-        auto res = GetDevInstProperty(dnDevInst, DEVPKEY_Device_PDOName);
+        auto res = dev.GetDevInstProperty(DEVPKEY_Device_PDOName);
 
         pdoPath = L"\\\\?\\GLOBALROOT"; // PDO prefix
         pdoPath += std::get<std::wstring>(res); // append PDO name
