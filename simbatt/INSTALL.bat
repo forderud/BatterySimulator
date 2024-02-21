33ada6ff76ec1631a001f3e658978900b72ba050
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-21 15:40:26 +0100

    Add instructions for adding the driver certificate to the root & trustedpublisher stores.

--- simbatt/INSTALL.bat
+++ simbatt/INSTALL.bat
@@ -2,6 +2,10 @@
 :: Goto current directory
 cd /d "%~dp0"
 
+:: Install certificate for silent driver installation and loading (run from administative developer command prompt)
+:: certmgr.exe /add simbatt.cer /s /r localMachine root
+:: certmgr.exe /add simbatt.cer /s /r localMachine trustedpublisher
+
 :: Use DevCon for installation, since it allows providing HWID
 devcon.exe /r install simbatt.inf "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 
