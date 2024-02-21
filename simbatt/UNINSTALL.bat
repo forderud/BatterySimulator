81684cc12a347c3a13ca0eecf5a72f299f64abdc
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-21 13:53:16 +0100

    Replace DevCon with DevGen for device instance creation
    Fixes #7.

--- simbatt/UNINSTALL.bat
+++ simbatt/UNINSTALL.bat
@@ -3,10 +3,11 @@
 cd /d "%~dp0"
 
 :: Delete simulated battery
-devcon.exe /r remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
-::devgen /remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+devgen /remove "SWD\DEVGEN\1"
+devgen /remove "SWD\DEVGEN\2"
 :: TODO: Switch to PNPUTIL in Win11 21H2
-::pnputil.exe /remove-device /deviceid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+::pnputil.exe /remove-device /deviceid "SWD\DEVGEN\1"
+::pnputil.exe /remove-device /deviceid "SWD\DEVGEN\2"
 
 :: Uninstall driver
 pnputil.exe /delete-driver simbatt.inf /uninstall /force /reboot
