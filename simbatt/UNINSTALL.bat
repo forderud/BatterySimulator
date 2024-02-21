464f406394f405aa1b0342b1b741a837edeee9cc
Fredrik Orderud <fredrik.orderud@ge.com>
2024-02-21 15:38:11 +0100

    Reorder uninstall steps.

--- simbatt/UNINSTALL.bat
+++ simbatt/UNINSTALL.bat
@@ -2,13 +2,13 @@
 :: Goto current directory
 cd /d "%~dp0"
 
-:: delete the devnode
+:: Delete simulated battery
 devcon.exe /r remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+::devgen /remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 :: TODO: Switch to PNPUTIL in Win11 21H2
 ::pnputil.exe /remove-device /deviceid "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
 
-:: uninstall driver
-::devgen /remove "{6B34C467-CE1F-4c0d-A3E4-F98A5718A9D6}\SimBatt"
+:: Uninstall driver
 pnputil.exe /delete-driver simbatt.inf /uninstall /force /reboot
 
 pause
