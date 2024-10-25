7e810178d1bc01d3c4c2caa60551f6940f41a623
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-10-25 13:50:11 +0200

    Add support for instead enumerating _interfaces_.

--- DevicePowerQuery/Main.cpp
+++ DevicePowerQuery/Main.cpp
@@ -6,6 +6,7 @@
 #include <Devpkey.h>
 #include "DeviceParams.hpp"
 #include "PowerData.hpp"
+#include <Usbiodef.h> // for GUID_DEVINTERFACE_USB_DEVICE
 
 #pragma comment (lib, "SetupAPI.lib")
 
@@ -68,15 +69,63 @@ int EnumerateDevices(GUID ClassGuid, DeviceVisitor visitor) {
     return idx;
 }
 
+int EnumerateInterfaces(GUID ClassGuid, DeviceVisitor visitor) {
+    HDEVINFO devInfo = SetupDiGetClassDevsW(&ClassGuid, 0, 0, DIGCF_DEVICEINTERFACE | DIGCF_PRESENT);
+    assert(devInfo != INVALID_HANDLE_VALUE);
 
-int main() {
-    GUID ClassGuid{};
-#ifdef ONLY_USB_DEVICES
-    // "USB Device" device setup class
-    wchar_t USBDevice_str[] = L"{88bae032-5a81-49f0-bc3d-a4ff138216d6}";
-    CLSIDFromString(USBDevice_str, &ClassGuid);
+    // iterate over all interfaces
+    int idx = 0;
+    for (; ; idx++) {
+        SP_DEVICE_INTERFACE_DATA interfaceData{};
+        interfaceData.cbSize = sizeof(interfaceData);
+        BOOL ok = SetupDiEnumDeviceInterfaces(devInfo, nullptr, &ClassGuid, idx, &interfaceData);
+        if (!ok) {
+            DWORD err = GetLastError();
+            if (err == ERROR_NO_MORE_ITEMS)
+                break;
+            abort();
+        }
+
+        SP_DEVINFO_DATA devInfoData = {};
+        devInfoData.cbSize = sizeof(devInfoData);
+        unsigned long deviceInfoDataSize = 0;
+        ok = SetupDiGetDeviceInterfaceDetailW(devInfo, &interfaceData, nullptr, 0, &deviceInfoDataSize, &devInfoData);
+        if (!ok) {
+            DWORD err = GetLastError();
+            assert(err == ERROR_INSUFFICIENT_BUFFER);
+        }
+
+#if 0
+        std::unique_ptr<SP_DEVICE_INTERFACE_DETAIL_DATA, decltype(&free)> detailData{ static_cast<SP_DEVICE_INTERFACE_DETAIL_DATA*>(malloc(deviceInfoDataSize)), &free };
+        detailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+
+        ok = SetupDiGetDeviceInterfaceDetailW(hDevInfo, &interfaceData, detailData.get(), deviceInfoDataSize, &deviceInfoDataSize, &devInfo);
+        if (!ok) {
+            DWORD err = GetLastError();
+            assert(err == ERROR_INSUFFICIENT_BUFFER);
+        }
 #endif
 
-    EnumerateDevices(ClassGuid, VisitDevicePowerData);
+        visitor(idx, devInfo, devInfoData);
+}
+
+    SetupDiDestroyDeviceInfoList(devInfo);
+    return idx;
+}
+
+
+int main() {
+    bool enumDevices = false;
+    if (enumDevices) {
+        GUID ClassGuid{};
+#ifdef ONLY_USB_DEVICES
+        // "USB Device" device setup class
+        wchar_t USBDevice_str[] = L"{88bae032-5a81-49f0-bc3d-a4ff138216d6}";
+        CLSIDFromString(USBDevice_str, &ClassGuid);
+#endif
+        EnumerateDevices(ClassGuid, VisitDevicePowerData);
+    } else {
+        EnumerateInterfaces(GUID_DEVINTERFACE_USB_DEVICE, VisitDevicePowerData);
+    }
     return 0;
 }
