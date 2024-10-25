e8298b1c2aa532207174f16476f830ed7d90323c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-10-25 13:28:56 +0200

    Refactor into visitor pattern.

--- DevicePowerQuery/Main.cpp
+++ DevicePowerQuery/Main.cpp
@@ -9,8 +9,37 @@
 
 #pragma comment (lib, "SetupAPI.lib")
 
+typedef void (*DeviceVisitor)(int idx, HDEVINFO hDevInfo, SP_DEVINFO_DATA devInfo);
 
-int EnumerateDevices(GUID ClassGuid, bool printPowerData) {
+
+void VisitDevicePlain(int idx, HDEVINFO hDevInfo, SP_DEVINFO_DATA devInfo) {
+    wprintf(L"\n");
+    wprintf(L"== Device %i: %s ==\n", idx, GetDevRegPropStr(hDevInfo, devInfo, SPDRP_FRIENDLYNAME).c_str());
+    wprintf(L"Description: %s\n", GetDevRegPropStr(hDevInfo, devInfo, SPDRP_DEVICEDESC).c_str());
+    wprintf(L"HWID       : %s\n", GetDevRegPropStr(hDevInfo, devInfo, SPDRP_HARDWAREID).c_str()); // HW type ID
+    wprintf(L"InstanceID : %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_InstanceId).c_str()); // HWID with instance suffix
+    wprintf(L"PDO        : %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_PDOName).c_str()); // Physical Device Object
+    wprintf(L"\n");
+}
+
+void VisitDevicePowerData(int idx, HDEVINFO hDevInfo, SP_DEVINFO_DATA devInfo) {
+    VisitDevicePlain(idx, hDevInfo, devInfo);
+
+    // TODO: Also query DEVPKEY_Device_PowerRelations
+
+    CM_POWER_DATA powerData = {};
+    BOOL ok = SetupDiGetDeviceRegistryPropertyW(hDevInfo, &devInfo, SPDRP_DEVICE_POWER_DATA, nullptr, (BYTE*)&powerData, sizeof(powerData), nullptr);
+    if (ok) {
+        PrintPowerData(powerData);
+    } else {
+        DWORD err = GetLastError();
+        err;
+        abort();
+    }
+}
+
+
+int EnumerateDevices(GUID ClassGuid, DeviceVisitor visitor) {
     DWORD flags = DIGCF_PRESENT;
     if (ClassGuid == GUID_NULL)
         flags |= DIGCF_ALLCLASSES; // query all connected devices
@@ -31,27 +60,7 @@ int EnumerateDevices(GUID ClassGuid, bool printPowerData) {
             abort();
         }
 
-        wprintf(L"\n");
-        wprintf(L"== Device %i: %s ==\n", idx, GetDevRegPropStr(hDevInfo, devInfo, SPDRP_FRIENDLYNAME).c_str());
-        wprintf(L"Description: %s\n", GetDevRegPropStr(hDevInfo, devInfo, SPDRP_DEVICEDESC).c_str());
-        wprintf(L"HWID       : %s\n", GetDevRegPropStr(hDevInfo, devInfo, SPDRP_HARDWAREID).c_str()); // HW type ID
-        wprintf(L"InstanceID : %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_InstanceId).c_str()); // HWID with instance suffix
-        wprintf(L"PDO        : %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_PDOName).c_str()); // Physical Device Object
-        wprintf(L"\n");
-
-        if (printPowerData) {
-            // TODO: Also query DEVPKEY_Device_PowerRelations
-
-            CM_POWER_DATA powerData = {};
-            ok = SetupDiGetDeviceRegistryPropertyW(hDevInfo, &devInfo, SPDRP_DEVICE_POWER_DATA, nullptr, (BYTE*)&powerData, sizeof(powerData), nullptr);
-            if (ok) {
-                PrintPowerData(powerData);
-            } else {
-                DWORD err = GetLastError();
-                err;
-                abort();
-            }
-        }
+        visitor(idx, hDevInfo, devInfo);
     }
 
     SetupDiDestroyDeviceInfoList(hDevInfo);
@@ -68,6 +77,6 @@ int main() {
     CLSIDFromString(USBDevice_str, &ClassGuid);
 #endif
 
-    EnumerateDevices(ClassGuid, true);
+    EnumerateDevices(ClassGuid, VisitDevicePowerData);
     return 0;
 }
