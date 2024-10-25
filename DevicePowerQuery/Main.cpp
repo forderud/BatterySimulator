1df434f672cd1e5aa3bf0c1cde7776bf7b6192ea
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-10-25 08:25:31 +0200

    Rename function to EnumerateDevices.

--- DevicePowerQuery/Main.cpp
+++ DevicePowerQuery/Main.cpp
@@ -97,7 +97,7 @@ static std::wstring GetDevPropStr(HDEVINFO hDevInfo, SP_DEVINFO_DATA& devInfo, c
     return result;
 }
 
-int GetDeviceDriverPowerData() {
+int EnumerateDevices(bool printPowerData) {
     // query all connected devices
     GUID ClassGuid{}; // no filtering
     HDEVINFO hDevInfo = SetupDiGetClassDevsW(&ClassGuid, 0, 0, DIGCF_ALLCLASSES | DIGCF_PRESENT);
@@ -116,23 +116,25 @@ int GetDeviceDriverPowerData() {
             abort();
         }
 
-        wprintf(L"\n== Device %i: %s ==\n", idx, GetDevRegPropStr(hDevInfo, devInfo, SPDRP_DEVICEDESC).c_str()); // SPDRP_FRIENDLYNAME or SPDRP_DEVICEDESC
+        wprintf(L"\n");
+        wprintf(L"== Device %i: %s ==\n", idx, GetDevRegPropStr(hDevInfo, devInfo, SPDRP_DEVICEDESC).c_str()); // SPDRP_FRIENDLYNAME or SPDRP_DEVICEDESC
         wprintf(L"HWID      : %s\n", GetDevRegPropStr(hDevInfo, devInfo, SPDRP_HARDWAREID).c_str()); // HW type ID
         wprintf(L"InstanceID: %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_InstanceId).c_str()); // HWID with instance suffix
         wprintf(L"PDO       : %s\n", GetDevPropStr(hDevInfo, devInfo, &DEVPKEY_Device_PDOName).c_str()); // Physical Device Object
+        wprintf(L"\n");
 
-#if 1
-        // TODO: Also query DEVPKEY_Device_PowerRelations
+        if (printPowerData) {
+            // TODO: Also query DEVPKEY_Device_PowerRelations
 
-        CM_POWER_DATA powerData = {};
-        ok = SetupDiGetDeviceRegistryPropertyW(hDevInfo, &devInfo, SPDRP_DEVICE_POWER_DATA, nullptr, (BYTE*)&powerData, sizeof(powerData), nullptr);
-        if (!ok) {
-            DWORD res = GetLastError(); res;
-            continue;
+            CM_POWER_DATA powerData = {};
+            ok = SetupDiGetDeviceRegistryPropertyW(hDevInfo, &devInfo, SPDRP_DEVICE_POWER_DATA, nullptr, (BYTE*)&powerData, sizeof(powerData), nullptr);
+            if (!ok) {
+                DWORD res = GetLastError(); res;
+                continue;
+            }
+
+            PrintPowerData(powerData);
         }
-
-        PrintPowerData(powerData);
-#endif
     }
 
     SetupDiDestroyDeviceInfoList(hDevInfo);
@@ -140,6 +142,8 @@ int GetDeviceDriverPowerData() {
     return 0;
 }
 
+
 int main() {
-    GetDeviceDriverPowerData();
+    EnumerateDevices(true);
+    return 0;
 }
