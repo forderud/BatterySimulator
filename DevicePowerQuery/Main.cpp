861b1230c4618990d829530bc1014f1ea4f95bab
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-11-03 15:28:33 +0000

    Extend USB Device class doc.

--- DevicePowerQuery/Main.cpp
+++ DevicePowerQuery/Main.cpp
@@ -152,7 +152,7 @@ int wmain(int argc, wchar_t* argv[]) {
             device_class = GUID_NULL; // devices mode (DOES includes logical devices beneath a composite USB device)
             interface_class = GUID_NULL; // (empty search)
         } else if (arg == L"--usb") {
-            device_class = ToGUID(L"{88bae032-5a81-49f0-bc3d-a4ff138216d6}"); // "USB Device" device setup class (DOES includes logical devices beneath a composite USB device)
+            device_class = ToGUID(L"{88bae032-5a81-49f0-bc3d-a4ff138216d6}"); // "USB Device" device setup class for custom devices that doesn't belong to another class (DOES includes logical devices beneath a composite USB device)
             interface_class = GUID_DEVINTERFACE_USB_DEVICE; // physical USB devices (does _not_ include logical devices beneath a composite USB device)
         } else if (arg == L"--hid") {
             device_class = ToGUID(L"{745a17a0-74d3-11d0-b6fe-00a0c90f57da}"); // "HID Device" device setup class
