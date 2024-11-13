04d2fed20c54d50604c8be0a4ae01885c2e30cf2
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-11-13 14:19:44 +0100

    Move device enumeration functions to a new header.

---
+++ DevicePowerQuery/DeviceEnum.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include <windows.h>
+#include <setupapi.h>
+#include <cassert>
+#include <iostream>
+#include "DeviceParams.hpp"
+
+#pragma comment (lib, "SetupAPI.lib")
+
+
+typedef void (*DeviceVisitor)(int idx, HDEVINFO devInfo, SP_DEVINFO_DATA& devInfoData);
+typedef int (*EnumerateFunction)(GUID classGuid, DeviceVisitor visitor);
+
+
+int EnumerateDevices(GUID classGuid, DeviceVisitor visitor) {
+    DWORD flags = DIGCF_PRESENT;
+    if (classGuid == GUID_NULL)
+        flags |= DIGCF_ALLCLASSES; // query all connected devices
+
+    HDEVINFO devInfo = SetupDiGetClassDevsW(&classGuid, 0, 0, flags);
+    assert(devInfo != INVALID_HANDLE_VALUE);
+
+    // iterate over all devices
+    int idx = 0;
+    for (; ; idx++) {
+        SP_DEVINFO_DATA devInfoData = {};
+        devInfoData.cbSize = sizeof(devInfoData);
+        BOOL ok = SetupDiEnumDeviceInfo(devInfo, idx, &devInfoData);
+        if (!ok) {
+            DWORD err = GetLastError();
+            if (err == ERROR_NO_MORE_ITEMS)
+                break;
+            abort();
+        }
+
+        visitor(idx, devInfo, devInfoData);
+    }
+
+    SetupDiDestroyDeviceInfoList(devInfo);
+    return idx;
+}
+
+int EnumerateInterfaces(GUID classGuid, DeviceVisitor visitor) {
+    DWORD flags = DIGCF_DEVICEINTERFACE | DIGCF_PRESENT;
+    //if (classGuid == GUID_NULL)
+    //    flags |= DIGCF_ALLCLASSES;
+
+    HDEVINFO devInfo = SetupDiGetClassDevsW(&classGuid, 0, 0, flags);
+    assert(devInfo != INVALID_HANDLE_VALUE);
+
+    // iterate over all interfaces
+    int idx = 0;
+    for (; ; idx++) {
+        SP_DEVICE_INTERFACE_DATA interfaceData{};
+        interfaceData.cbSize = sizeof(interfaceData);
+        BOOL ok = SetupDiEnumDeviceInterfaces(devInfo, nullptr, &classGuid, idx, &interfaceData);
+        if (!ok) {
+            DWORD err = GetLastError();
+            if (err == ERROR_NO_MORE_ITEMS)
+                break;
+            abort();
+        }
+
+        SP_DEVINFO_DATA devInfoData = {};
+        devInfoData.cbSize = sizeof(devInfoData);
+        ok = SetupDiGetDeviceInterfaceDetailW(devInfo, &interfaceData, nullptr, 0, nullptr, &devInfoData);
+        if (!ok) {
+            DWORD err = GetLastError();
+            assert(err == ERROR_INSUFFICIENT_BUFFER);
+        }
+
+        visitor(idx, devInfo, devInfoData);
+
+        wprintf(L"DeviceInterfacePath: %s\n", GetDevicePath(devInfo, interfaceData).c_str()); // can be passsed to CreateFile
+    }
+
+    SetupDiDestroyDeviceInfoList(devInfo);
+    return idx;
+}
--- DevicePowerQuery/Main.cpp
+++ DevicePowerQuery/Main.cpp
@@ -1,20 +1,12 @@
-#include <windows.h>
-#include <setupapi.h>
-#include <cassert>
-#include <iostream>
+#include "DeviceEnum.hpp"
 #include <initguid.h>
 #include <Devpkey.h>
-#include "DeviceParams.hpp"
 #include "PowerData.hpp"
 #include <Devguid.h>  // for GUID_DEVCLASS_*
 #include <Batclass.h> // for GUID_DEVICE_BATTERY
 #include <Hidclass.h> // for GUID_DEVINTERFACE_HID
 #include <Usbiodef.h> // for GUID_DEVINTERFACE_USB_DEVICE
 
-#pragma comment (lib, "SetupAPI.lib")
-
-typedef void (*DeviceVisitor)(int idx, HDEVINFO devInfo, SP_DEVINFO_DATA& devInfoData);
-typedef int (*EnumerateFunction)(GUID classGuid, DeviceVisitor visitor);
 
 GUID ToGUID(const wchar_t* str) {
     GUID guid{};
@@ -54,72 +46,6 @@ void VisitDevicePowerData(int idx, HDEVINFO devInfo, SP_DEVINFO_DATA& devInfoDat
         err;
         abort();
     }
-}
-
-int EnumerateDevices(GUID classGuid, DeviceVisitor visitor) {
-    DWORD flags = DIGCF_PRESENT;
-    if (classGuid == GUID_NULL)
-        flags |= DIGCF_ALLCLASSES; // query all connected devices
-
-    HDEVINFO devInfo = SetupDiGetClassDevsW(&classGuid, 0, 0, flags);
-    assert(devInfo != INVALID_HANDLE_VALUE);
-
-    // iterate over all devices
-    int idx = 0;
-    for (; ; idx++) {
-        SP_DEVINFO_DATA devInfoData = {};
-        devInfoData.cbSize = sizeof(devInfoData);
-        BOOL ok = SetupDiEnumDeviceInfo(devInfo, idx, &devInfoData);
-        if (!ok) {
-            DWORD err = GetLastError();
-            if (err == ERROR_NO_MORE_ITEMS)
-                break;
-            abort();
-        }
-
-        visitor(idx, devInfo, devInfoData);
-    }
-
-    SetupDiDestroyDeviceInfoList(devInfo);
-    return idx;
-}
-
-int EnumerateInterfaces(GUID classGuid, DeviceVisitor visitor) {
-    DWORD flags = DIGCF_DEVICEINTERFACE | DIGCF_PRESENT;
-    //if (classGuid == GUID_NULL)
-    //    flags |= DIGCF_ALLCLASSES;
-
-    HDEVINFO devInfo = SetupDiGetClassDevsW(&classGuid, 0, 0, flags);
-    assert(devInfo != INVALID_HANDLE_VALUE);
-
-    // iterate over all interfaces
-    int idx = 0;
-    for (; ; idx++) {
-        SP_DEVICE_INTERFACE_DATA interfaceData{};
-        interfaceData.cbSize = sizeof(interfaceData);
-        BOOL ok = SetupDiEnumDeviceInterfaces(devInfo, nullptr, &classGuid, idx, &interfaceData);
-        if (!ok) {
-            DWORD err = GetLastError();
-            if (err == ERROR_NO_MORE_ITEMS)
-                break;
-            abort();
-        }
-
-        SP_DEVINFO_DATA devInfoData = {};
-        devInfoData.cbSize = sizeof(devInfoData);
-        ok = SetupDiGetDeviceInterfaceDetailW(devInfo, &interfaceData, nullptr, 0, nullptr, &devInfoData);
-        if (!ok) {
-            DWORD err = GetLastError();
-            assert(err == ERROR_INSUFFICIENT_BUFFER);
-        }
-
-        visitor(idx, devInfo, devInfoData);
-
-        wprintf(L"DeviceInterfacePath: %s\n", GetDevicePath(devInfo, interfaceData).c_str()); // can be passsed to CreateFile
-    }
-
-    SetupDiDestroyDeviceInfoList(devInfo);
-    return idx;
 }
 
 
