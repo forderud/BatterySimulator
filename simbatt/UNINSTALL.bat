258ea579d64f235dc9d2b70cdb1d3eb1a30a0c0c
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-05 18:03:30 +0100

    Change HWID to HID_DEVICE_UP:0084_U:0004
    This makes the driver load when attaching a HID Power Device.

--- simbatt/UNINSTALL.bat
+++ simbatt/UNINSTALL.bat
@@ -2,13 +2,6 @@
 :: Goto current directory
 cd /d "%~dp0"
 
-:: Delete simulated battery
-devgen /remove "SWD\DEVGEN\1"
-devgen /remove "SWD\DEVGEN\2"
-:: TODO: Switch to PNPUTIL in Win11 21H2
-::pnputil.exe /remove-device /deviceid "SWD\DEVGEN\1"
-::pnputil.exe /remove-device /deviceid "SWD\DEVGEN\2"
-
 :: Uninstall driver
 pnputil.exe /delete-driver simbatt.inf /uninstall /force
 
