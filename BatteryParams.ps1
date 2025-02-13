9cbcfca0dfe01eacba0a2bd7d8774d1c887e9591
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-13 19:41:44 +0100

    Also query MSBatteryClass parameters, that includes BatteryCycleCount.

--- BatteryParams.ps1
+++ BatteryParams.ps1
@@ -3,3 +3,6 @@
 
 Write-Host "Parameters for connected batteries:"
 Get-CimInstance -Namespace root\CIMV2 -Class Win32_Battery
+
+Write-Host "Extra parameters for connected batteries:"
+Get-WmiObject -Namespace root\wmi  -Class MSBatteryClass
