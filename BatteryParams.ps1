ad6aac0f6c0e23ddd3aee6979ebbebcd19bad75f
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-02-13 19:48:53 +0100

    Document that CycleCount is a readonly parameter.

--- BatteryParams.ps1
+++ BatteryParams.ps1
@@ -5,7 +5,7 @@ Write-Host "Parameters for connected batteries:"
 Get-CimInstance -Namespace root\CIMV2 -Class Win32_Battery
 
 Write-Host "Extra parameters for connected batteries:"
-Get-WmiObject -Namespace root\wmi  -Class MSBatteryClass
+Get-WmiObject -Namespace root\wmi -Class MSBatteryClass
 
-$cc = Get-WmiObject -Namespace root\wmi  -Class BatteryCycleCount
-Write-Host Battery cycle count: $cc.CycleCount
+$cc = Get-CimInstance -Namespace root\wmi  -Class BatteryCycleCount
+Write-Host Battery cycle count: $cc.CycleCount " (readonly parameter)"
