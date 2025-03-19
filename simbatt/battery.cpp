a9a44661d57c7c8e3ce0b5e198bc44756cc68928
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-19 17:06:35 +0100

    Increase battery temperature to 999 Kelvin (725 Centigrade)
    Done in an attempt to trigger a "critical thermal event" in Windows. However, nothing special seem to happen and I'm not able to find any associated log errors.

--- simbatt/battery.cpp
+++ simbatt/battery.cpp
@@ -123,7 +123,7 @@ Arguments:
 
         DevExt->State.EstimatedTime = BATTERY_UNKNOWN_TIME; // battery run time, in seconds
 
-        DevExt->State.Temperature = 2981; // 25 degree Celsius [10ths of a degree Kelvin]
+        DevExt->State.Temperature = 999*10; // [10ths of a degree Kelvin]
 
         RtlStringCchCopyW(DevExt->State.DeviceName, MAX_BATTERY_STRING_SIZE, L"SimulatedBattery");
 
