5b700c4f92aa790cf1c7fe4c4af24df78b772aba
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-03-19 16:33:51 +0100

    reduce indentation and add unit doc to EstimatedTime & Temperature parameters.

--- simbatt/battery.hpp
+++ simbatt/battery.hpp
@@ -7,29 +7,29 @@ extern "C" {
 
 
 struct BATT_STATE {
-    BATTERY_MANUFACTURE_DATE        ManufactureDate;
-    BATTERY_INFORMATION             BatteryInfo;
-    BATTERY_STATUS                  BatteryStatus;
-    ULONG                           GranularityCount;
-    BATTERY_REPORTING_SCALE         GranularityScale[4];
-    ULONG                           EstimatedTime;
-    ULONG                           Temperature;
-    WCHAR                           DeviceName[MAX_BATTERY_STRING_SIZE];
-    WCHAR                           ManufacturerName[MAX_BATTERY_STRING_SIZE];
-    WCHAR                           SerialNumber[MAX_BATTERY_STRING_SIZE];
-    WCHAR                           UniqueId[MAX_BATTERY_STRING_SIZE];
+    BATTERY_MANUFACTURE_DATE ManufactureDate;
+    BATTERY_INFORMATION      BatteryInfo;
+    BATTERY_STATUS           BatteryStatus;
+    ULONG                    GranularityCount;
+    BATTERY_REPORTING_SCALE  GranularityScale[4];
+    ULONG                    EstimatedTime; // seconds
+    ULONG                    Temperature;   // 10ths of a degree Kelvin
+    WCHAR                    DeviceName[MAX_BATTERY_STRING_SIZE];
+    WCHAR                    ManufacturerName[MAX_BATTERY_STRING_SIZE];
+    WCHAR                    SerialNumber[MAX_BATTERY_STRING_SIZE];
+    WCHAR                    UniqueId[MAX_BATTERY_STRING_SIZE];
 };
 
 struct DEVICE_CONTEXT {
     // Battery class registration
-    void*                           ClassHandle;
-    WDFWAITLOCK                     ClassInitLock;
-    WMILIB_CONTEXT                  WmiLibContext;
+    void*                    ClassHandle;
+    WDFWAITLOCK              ClassInitLock;
+    WMILIB_CONTEXT           WmiLibContext;
 
     // Battery state
-    WDFWAITLOCK                     StateLock;
-    ULONG                           BatteryTag;
-    BATT_STATE                      State;
+    WDFWAITLOCK              StateLock;
+    ULONG                    BatteryTag;
+    BATT_STATE               State;
 };
 
 WDF_DECLARE_CONTEXT_TYPE(DEVICE_CONTEXT);
