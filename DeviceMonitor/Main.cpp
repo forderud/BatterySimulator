6c0791f1bd9cb1fb48c13d24f23e21c9d4c217db
Fredrik Orderud <forderud@gmail.com>
2026-06-23 21:01:33 +0200

    .

--- DeviceMonitor/Main.cpp
+++ DeviceMonitor/Main.cpp
@@ -14,53 +14,33 @@ void LogError (const wchar_t* message) {
 }
 
 
-INT_PTR WINAPI WinProcCallback(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-// Routine Description:
-//     Simple Windows callback for handling messages.
-//     This is where all the work is done because the example
-//     is using a window to process messages. This logic would be handled 
-//     differently if registering a service instead of a window.
-
-// Parameters:
-//     hWnd - the window handle being registered for events.
-
-//     message - the message being interpreted.
-
-//     wParam and lParam - extended information provided to this
-//          callback by the message sender.
-
-//     For more information regarding these parameters and return value,
-//     see the documentation for WNDCLASSEX and CreateWindowEx.
-{
-    LRESULT lRet = 1;
-
-    switch (message)
-    {
+INT_PTR WINAPI WinProcCallback(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
     case WM_DEVICECHANGE:
     {
-        // This is the actual message from the interface via Windows messaging.
-        // This code includes some additional decoding for this particular device type
-        // and some common validation checks.
-        //
-        // Note that not all devices utilize these optional parameters in the same
-        // way. Refer to the extended information for your particular device type 
-        // specified by your GUID.
-        auto b = (DEV_BROADCAST_DEVICEINTERFACE_W*)lParam;
+        auto hdr = (DEV_BROADCAST_HDR*)lParam;
 
         switch (wParam) {
         case DBT_DEVICEARRIVAL:
         {
-            assert(b->dbcc_devicetype == DBT_DEVTYP_PORT); // TODO: Should have been DBT_DEVTYP_DEVICEINTERFACE
-            WCHAR guid_str[39];
-            StringFromGUID2(b->dbcc_classguid, guid_str, 39);
-            wprintf(L"Message: DBT_DEVICEARRIVAL (class: %s, name %s)\n", guid_str, b->dbcc_name); // TODO: Name appear corrupted
+            if (hdr->dbch_devicetype == DBT_DEVTYP_PORT) {
+                auto hdr_port = (DEV_BROADCAST_PORT_W*)lParam;
+                wprintf(L"Message: DBT_DEVICEARRIVAL DBT_DEVTYP_PORT (name: %s)\n", hdr_port->dbcp_name);
+            } else if (hdr->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
+                auto hdr_di = (DEV_BROADCAST_DEVICEINTERFACE_W*)lParam;
+                wchar_t guid_str[39]{};
+                StringFromGUID2(hdr_di->dbcc_classguid, guid_str, 39);
+                wprintf(L"Message: DBT_DEVICEARRIVAL DBT_DEVTYP_DEVICEINTERFACE (class: %s, name %s)\n", guid_str, hdr_di->dbcc_name);
+            } else {
+                wprintf(L"Message: DBT_DEVICEARRIVAL unknown type %Iu\n", wParam);
+            }
             break;
         }
         case DBT_DEVICEREMOVECOMPLETE:
             wprintf(L"Message: DBT_DEVICEREMOVECOMPLETE\n");
             break;
         case DBT_DEVNODES_CHANGED:
-            wprintf(L"Message: DBT_DEVNODES_CHANGED\n");
+            //wprintf(L"Message: DBT_DEVNODES_CHANGED\n");
             break;
         default:
             wprintf(L"Message: WM_DEVICECHANGE message received, value %Iu unhandled.\n", wParam);
@@ -70,11 +50,11 @@ INT_PTR WINAPI WinProcCallback(HWND hWnd, UINT message, WPARAM wParam, LPARAM lP
     }
     default:
         // Send all other messages on to the default windows handler.
-        lRet = DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hWnd, message, wParam, lParam);
         break;
     }
 
-    return lRet;
+    return 1;
 }
 
 
